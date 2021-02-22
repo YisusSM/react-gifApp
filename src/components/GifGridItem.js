ef377ecd617f1eae34f621c70773808a8d44779d
TheBlockCode <jesussm_0001@hotmail.com>
2021-02-22 12:41:27 -0600

    card css

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -4,7 +4,7 @@ export const GifGridItem = ( { id, title, url } ) => {
 
     
     return (
-        <div className = "animate__animated animate__bounce animate__fadeInLeft">
+        <div className = "card animate__animated animate__bounce animate__fadeInLeft">
            <img src = { url } alt = { title } />
            <p> { title } </p>
         </div>
